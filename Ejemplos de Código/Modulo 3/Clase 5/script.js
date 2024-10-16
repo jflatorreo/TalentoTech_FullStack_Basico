809c10a814ce8f602bf434e4fc55036010ae10fa
jflatorreo <jflatorreo@unal.edu.co>
2024-10-15 21:22:24 -0500

    Ejemplo Punto 1 - Taller Clase 5 Modulo 3

---
+++ Ejemplos de Código/Modulo 3/Clase 5/script.js
@@ -0,0 +1,20 @@
+function generarHistoria(nombre,lugar,objeto){
+    /* Tu código para generar la historia... Ej..
+    
+    let historia= "Estaba " + nombre + " en" + lugar + " jugando con un "+objeto;
+    
+    o así:
+
+    let historia = `Estaba ${nombre} en ${lugar} jugando con un ${objeto}`
+    */
+
+    return historia
+}
+
+let nombre="maria";
+let lugar="bogota";
+let objeto="dados";
+
+historia = generarHistoria(nombre,lugar,objeto);
+
+console.log(historia)
